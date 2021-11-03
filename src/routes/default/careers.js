a25bdc2250b0071424ffc72ccf966ce8f88ebe9d
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-11-03 16:00:32 -0500

    commit

--- src/routes/default/careers.js
+++ src/routes/default/careers.js
@@ -1,45 +1,10 @@
-import Box from '@spraoi/base/Box';
 import React from 'react';
-import ContactCTA from '../../components/ContactCTA';
-import LinkList from '../../components/LinkList';
-import Section from '../../components/Section';
+
 
 const Careers = () => {
 
   return (
-    <>
-      <Section sx={{ textAlign: [null, null, 'center'] }}>
-        <Box as="h1" sx={{ fontSize: [5, null, 6] }}>
-          Announcements
-        </Box>
-        <Box
-          as="p"
-          sx={{
-            fontSize: 4,
-            lineHeight: 1,
-            maxWidth: 'maxWidths.paragraph',
-            mt: 5,
-            mx: 'auto',
-          }}
-        >
-          News travels fast these days. Stay apprised of all things
-          Spraoi&nbsp;here.
-        </Box>
-      </Section>
-      <Section sx={{ px: [null, null, 7] }}>
-        <LinkList
-          items={articles
-            .filter((article) => article.frontmatter.isAnnouncement)
-            .map((article) => ({
-              link: `/announcements${article.fields.slug}`,
-              subText: article.frontmatter.datePublished,
-              title: article.frontmatter.title,
-            }))}
-          titleAs="h2"
-        />
-      </Section>
-      <ContactCTA />
-    </>
+    <div></div>
   );
 };
 
