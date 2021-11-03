1af58e8f3dd4daf486116ad9556351f7dfec2270
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-11-03 11:22:02 -0500

    commit test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -118,7 +118,7 @@ const Footer = () => (
             },
             {
               icon: 'instagram',
-              link: 'https://www.instagram.com/shockmousemedia',
+              link: 'https://www.instagram.com/shockmousemedi',
             },
           ].map((item) => (
             <Box key={item.link} as="li" sx={{}}>
