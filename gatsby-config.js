0588f26ed717dd6fc2a8b92084115695b080b0fc
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-11-08 13:51:46 -0600

    property id analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-XDTHKG333J",
+        trackingId: "292710070",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
